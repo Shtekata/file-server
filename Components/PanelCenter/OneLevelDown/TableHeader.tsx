1f803420f512d306f5ed795ffad2d867ea2e5295
Shtekata <gesheval@gmail.com>
2026-05-31 23:18:04 +0300

    Correction to TableHeader

--- Components/PanelCenter/OneLevelDown/TableHeader.tsx
+++ Components/PanelCenter/OneLevelDown/TableHeader.tsx
@@ -1,19 +1,23 @@
-import clsx from 'clsx'
-
 export default function TableHeader({ canManage }: { canManage: boolean }) {
   return (
-    <div
-      className={clsx(
-        'grid grid-cols-48 border-b border-zinc-200 py-3 text-sm text-zinc-500 dark:border-white/10 dark:text-zinc-400',
-        canManage ? 'px-3 sm:px-5' : 'px-5',
+    <>
+      {!canManage ? (
+        <div className='grid grid-cols-48 border-b border-zinc-200 py-3 text-sm text-zinc-500 dark:border-white/10 dark:text-zinc-400 px-5'>
+          <div className='col-span-18 sm:col-span-30'>Name</div>
+          <div className='col-span-10 sm:col-span-6'>Size</div>
+          <div className='col-span-10 sm:col-span-8'>Modified</div>
+          <div className='col-span-10 sm:col-span-4 text-center'>Action</div>
+        </div>
+      ) : (
+        <div className='grid grid-cols-48 grid-rows-2 px-3 sm:px-5 border-b border-zinc-200 text-sm text-zinc-500 dark:border-white/10 dark:text-zinc-400'>
+          <div className='col-span-48 row-start-1 border-b border-dashed py-3 border-zinc-200 dark:border-white/10'>
+            Name
+          </div>
+          <div className='col-span-7 col-start-19 sm:col-span-6 row-start-2 py-3'>Size</div>
+          <div className='col-span-9 col-start-26 sm:col-span-8 row-start-2 py-3'>Modified</div>
+          <div className='col-span-14 col-start-35 sm:col-span-11 row-start-2 py-3 text-center'>Action</div>
+        </div>
       )}
-    >
-      <div className={clsx('col-span-18', canManage ? ' sm:col-span-23' : ' sm:col-span-30')}>Name</div>
-      <div className={clsx('sm:col-span-6', canManage ? 'col-span-7' : 'col-span-10')}>Size</div>
-      <div className={clsx('sm:col-span-8', canManage ? 'col-span-9' : 'col-span-10')}>Modified</div>
-      <div className={clsx('text-center', canManage ? 'col-span-14 sm:col-span-11' : 'col-span-10 sm:col-span-4')}>
-        Action
-      </div>
-    </div>
+    </>
   )
 }
